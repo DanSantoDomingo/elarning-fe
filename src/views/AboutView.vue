d84fd39576cfcb91eefc8cfffc1c3eb2e864665a
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2023-02-08 14:27:27 +0800

    added about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1>Kung anong guston mong ilagay na description ng app</h1>
+  <div class="fs-4">
+    <p>
+      Introducing the Fractions Learning app! A comprehensive educational tool designed to help students master the concept of fractions. The app offers a range of lessons on different topics related to fractions, including addition, subtraction, multiplication, and division.
+    </p>
+    <p>
+      Students can select lessons that match their skill level and start learning with just a click of a button. The app features a play button that allows the browser to read out the lesson, making it easier for students to follow along.
+    </p>
+    <p>
+      Moreover, the app offers interactive quizzes after each lesson to help students test their understanding. The quizzes are designed to be fun and engaging, and they cover the material that has been taught in the lesson.
+    </p>
+    <p>
+      With this app, students can learn and reinforce their knowledge of fractions in a fun and engaging way. Whether they're just starting to learn about fractions or they want to brush up on their skills, the Fractions Learning app is an excellent resource for students of all ages.
+    </p>
+  </div>
 </template>
 
 <style></style>
