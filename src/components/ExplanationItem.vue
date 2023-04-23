c9cc89c9498f00713d98675a9855f018981c45a1
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2023-04-23 14:49:52 +0800

    udpates

--- src/components/ExplanationItem.vue
+++ src/components/ExplanationItem.vue
@@ -1,8 +1,20 @@
 <template>
-  <span>Question:</span>
-  <div v-html="question.content" />
-  <span>Solution:</span>
-  <div v-html="question.solution" />
+ 
+ 
+
+  <div
+    class="card border border-primary mt-3"
+  >
+    <div class="card-body">
+      <p class="card-text">
+        <span>Question:</span>
+        <span v-html="question.content" />
+ 
+        <span>Solution:</span>
+        <span v-html="question.solution" />
+      </p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
